faaec886cbbdc3c1d228587d3a43bdad5cafb9fb
Himanshu Chandra <himanshuchandra9808@gmail.com>
2023-01-10 13:03:27 +0530

    This App convert kg to pounds

---
+++ app/src/main/java/com/example/unconvert/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.unconvert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+     private Button button;
+     private TextView textView;
+     private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(MainActivity.this, "The Answer is in Text view", Toast.LENGTH_SHORT).show();
+                String s= editText.getText().toString();
+               int Kg = Integer.parseInt(s);
+               double Pound = 2.205 * Kg;
+               textView.setText("The coresponding value in pound is " + Pound);
+            }
+
+        }
+        );
+    }
+}
